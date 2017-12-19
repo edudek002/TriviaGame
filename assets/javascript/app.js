37cb55c870f94af11ae83e2700fc8c77266d4ad9
Edyta <dudek002@hotmail.com>
2017-12-18 21:32:17 -0600

    added next page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,71 +3,49 @@
 
 // This code will run as soon as the page loads
 
-
 window.onload =  function() {
-$("#start").on("click", stopwatch.start);
 
+  $("#start").on("click", stopwatch.start);
   //$("#lap").on("click", stopwatch.recordLap);
   $("#stop").on("click", stopwatch.stop);
   //$("#reset").on("click", stopwatch.reset);
-
 };
 
-
-
-
-
-//  Variable that will hold our setInterval that runs the stopwatch
+//  Variable that holds setInterval that runs the stopwatch
 var intervalId;
-
-//prevents the clock from being sped up unnecessarily
+//Variable that prevents the clock from being sped up unnecessarily
 var clockRunning = false;
+var seconds=0;
 
-// Our stopwatch object
+// STOPWATCH OBJECT
 var stopwatch = {
 
   time: 0,
   lap: 1,
-
-  reset: function() {
-
-    stopwatch.time = 0;
-    stopwatch.lap = 1;
-
-    // DONE: Change the "display" div to "120."
-    $("#display").text("120");
-
-    if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count, 1000);
-        clockRunning = true;
-    }
-
-   }, 
   
   start: function() {
 
-    // DONE: Use setInterval to start the count here and set the clock to running.
+    // SetInterval to start the count here and set the clock to running.
+    $("#display").text("Remaining time: 20 seconds.");
     if (!clockRunning) {
         intervalId = setInterval(stopwatch.count, 1000);
         clockRunning = true;
     }
   },
+
   stop: function() {
 
-    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    // ClearInterval to stop the count here and set the clock to not be running.
     clearInterval(intervalId);
     clockRunning = false;
   },
 
   count: function() {
 
-    // DONE: increment time by 1, remember we cant use "this" here.
+    // Increment time by 1
     stopwatch.time++;
-
-    var seconds = 20 - stopwatch.time;
-
+    seconds = 20 - stopwatch.time;
     console.log(seconds); 
-
     $("#display").text("Remaining time: " + seconds + " seconds.");
 
     if (stopwatch.time <20){
@@ -78,15 +56,13 @@ var stopwatch = {
       $("#information").text("Time out");
     }
   },
+}; //END OF OBJECT
 
-};
 
-  var radios = "";
-  var val= "";
-  var win=0;
 
-  
-
+var radios = "";
+var val= "";
+var win=0;
 
 var submitAnswer = function() {
 
@@ -104,20 +80,33 @@ var submitAnswer = function() {
     console.log('Answer is correct !');
     win++;
     stopwatch.stop();
-    $("#information").text("Your answer is correct");
+    
+    //delay 1 second
+    var windowTimeout = setTimeout(function(){
+        
+        $("#information").text("Your answer is correct");
 
     $("#wins").text("Wins = " + win);
-    //delay 1 second
+      }, 1000);
   } 
   else {
     console.log('Answer is wrong');
     win;
     stopwatch.stop();
-    $("#information").text("Your answer is incorrect");
+    
     //delay 1 second
+    var windowTimeout = setTimeout(function(){
+        
+        $("#information").text("Your answer is incorrect");
+
+
+      }, 1000);
   }
 
 };
+
+
+
 
 
 
